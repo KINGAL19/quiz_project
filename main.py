5b3beb475297fe2c4611b2c1557e49ba5c4f6b42
KINGAL <kingal171314@gmail.com>
2022-08-02 20:21:33 +0800

    question model and questions_list

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from question import Question
 from data import question_data
-from quiz_brain import Quiz_brain
+# from quiz_brain import Quiz_brain
 
+questions_list = []
+
+for d in question_data:
+    q = Question(d['text'], d['answer'])
+    questions_list.append(q)
+
+# print(question_list[0].text)
